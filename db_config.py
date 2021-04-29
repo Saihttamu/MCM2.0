022b872fd8e0544449057c036a30e8fb37f0d57f
Saihttamu <matthias.braneyre@gmail.com>
2021-04-29 11:13:55 +0100

    on tente la connection sur la machine locale

--- db_config.py
+++ db_config.py
@@ -6,6 +6,6 @@ mysql = MySQL()
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'api'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_DB'] = 'metao-api'
+app.config['MYSQL_DATABASE_HOST'] = '192.168.1.12'
 mysql.init_app(app)
