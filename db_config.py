a213bc02dfe26c1f5033cf3a6c530b4f0d03798b
Saihttamu <matthias.braneyre@gmail.com>
2021-04-29 10:50:40 +0100

    on repasse sur la base de données api

--- db_config.py
+++ db_config.py
@@ -6,6 +6,6 @@ mysql = MySQL()
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'meteo-api'
+app.config['MYSQL_DATABASE_DB'] = 'api'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
